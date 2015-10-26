ce4d9ce6b6f1e2524860a6a15e87e29e5473d140
Yauheni Zablotski <e.zablotski@gmail.com>
2015-10-27 00:32:19 +0100

    proof of concept

--- sag-three/src/main/java/info/zablotski/App.java
+++ sag-three/src/main/java/info/zablotski/App.java
@@ -1,13 +1,33 @@
 package info.zablotski;
 
+import org.apache.commons.io.IOUtil;
+import org.apache.commons.lang.StringUtils;
+
+import java.io.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter string: ");
+        String filename = br.readLine();
+        App app = new App();
+        app.printFileContent(filename);
+    }
+
+    private void printFileContent(String filename){
+        String output = "";
+        ClassLoader classLoader = getClass().getClassLoader();
+        try{
+            output = IOUtil.toString(classLoader.getResourceAsStream(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        output = StringUtils.deleteWhitespace(output);
+        System.out.println(output);
     }
 }
